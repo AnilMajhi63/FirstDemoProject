1cbb3db0d50294956f523d651d29af1e170033bb
AnilMajhi63 <soft.majhi@gmail.com>
2020-11-27 20:13:24 +0530

    added a new msg.

--- src/main/java/org/anil/restapi/messenger/service/MessageService.java
+++ src/main/java/org/anil/restapi/messenger/service/MessageService.java
@@ -15,7 +15,7 @@ public class MessageService {
 	{
 		message.put(1l, new Message(101, "core java", "anilmajhi"));
 		message.put(2l, new Message(102, "Rest Api", "pinku"));
-		message.put(3l, new Message(103, "Spring", "pinku"));
+		//message.put(3l, new Message(103, "Spring", "pinku"));45
 
 		message.put(3l, new Message(105, "php", "Sumant"));//Test1 Heigala checked in before 1 minute new changes 
 		//call kar
