49216bce0d3e118444dc4c4cf210fb095fa8dc81
AnilMajhi63 <75078048+AnilMajhi63@users.noreply.github.com>
2020-11-27 20:18:05 +0530

    Update MessageService.java

--- src/main/java/org/anil/restapi/messenger/service/MessageService.java
+++ src/main/java/org/anil/restapi/messenger/service/MessageService.java
@@ -13,7 +13,7 @@ public class MessageService {
 	
 	public MessageService()
 	{
-		message.put(1l, new Message(101, "core java", "anilmajhi"));
+		message.put(1l, new Message(101, "core java", "Anil weds Sabnam"));
 		message.put(2l, new Message(102, "Rest Api", "pinku"));
 		//message.put(3l, new Message(103, "Spring", "pinku"));45
 
